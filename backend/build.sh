c2f0847ffcd84c9df906822f7ee338c96a839246
arunkg005 <akgindian55@gmail.com>
2026-05-22 15:42:07 +0530

    Fix Render deploy: move django migrate command from build to start phase

--- backend/build.sh
+++ backend/build.sh
@@ -5,8 +5,6 @@ set -o errexit
 # Install dependencies
 pip install -r requirements.txt
 
-# Run migrations
-python manage.py migrate --noinput
-
 # Collect static files
 python manage.py collectstatic --noinput
+
